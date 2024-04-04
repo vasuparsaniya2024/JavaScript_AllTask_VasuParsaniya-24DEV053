7810f235bd03f9c646f0989bc97e1466e9beef66
Vasu-Parsaniya <vasuparsaniya2024@gmail.com>
2024-04-04 14:07:52 +0530

    routes folder login registration forgotpassword folder remove

--- index.js
+++ index.js
@@ -6,50 +6,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const registration = require('./routes/registration/registration.js');
-const login = require('./routes/login/login.js');
-const forgotpassword = require('./routes/forgotpassword/forgotpassword.js');
 
-const home = require('./routes/HomePage/Home.js');
 const javascripttask = require('./routes/javascripttask/javascripttask.js');
 
 
-// const pagginationcomponent = require('./routes/pagginationcomponent.js');
-// const insertStudentDetails = require('./routes/insertRecords.js');
-
-// const dynamiccombo = require('./routes/Task-15/DynamicCombo_exercise6/dynamiccombo.js') // remainning
-
-
-// const timezoneconverter = require('./routes/Task-19/timezonebycity/timezonecity.js');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }));
 
 //middleware 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //ejs file render we need to set path of located files
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
 app.use(cookieParser());
 
 //routes
-// app.use(registration);
-// app.use(login);
-// app.use(forgotpassword);
-
-// app.use(home);
 
 app.use(javascripttask);
 
-// app.use(dynamiccombo);
 
 
-// app.use(timezoneconverter);
 
 // app.use(insertStudentDetails);
 // app.use(user);
@@ -58,20 +39,20 @@ app.use(javascripttask);
 // app.use(studentAttandance);
 // app.use(studentResult);
 
-portfinder.getPort(function(err,port){
-        try{
-            if(err) throw err
-            app.listen(port,(error)=>{
-                console.log("Server Listen At "+port);
-            });
-        }catch(err){
-            console.log("Error In Server Listen: "+err);
-        }
+portfinder.getPort(function (err, port) {
+    try {
+        if (err) throw err
+        app.listen(port, (error) => {
+            console.log("Server Listen At " + port);
+        });
+    } catch (err) {
+        console.log("Error In Server Listen: " + err);
+    }
 });
 
 
 
 //put this at last because any route not found then execute this
-app.use((req,res)=>{
-    res.render('homepage',{message:"Not Found"});
+app.use((req, res) => {
+    res.render('homepage', { message: "Not Found" });
 });
